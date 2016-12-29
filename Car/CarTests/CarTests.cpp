c486cae6b40f249aa12c076c7511df8b03d677b3
DenisTuxvatullin <den44icc@mail.ru>
2016-12-29 12:37:44 +0300

    NewTests

--- Car/CarTests/CarTests.cpp
+++ Car/CarTests/CarTests.cpp
@@ -3,7 +3,8 @@
 
 #include "stdafx.h"
 #include "..\Car\Car.h"
-const int REVERSE = 6, NEUTRAL = 0, FIRST = 1, SECOND = 2, THIRD = 3, FOUTH = 4, FIFTH = 5;
+#include <boost/test/unit_test.hpp>
+const int REVERSE = 6, NEUTRAL = 0, FIRST = 1, SECOND = 2, THIRD = 3, FOUTH = 4, FIFTH = 5, FORWARD = 1, BACKWARD = -1;
 
 struct CarFixture
 {
@@ -12,38 +13,12 @@ struct CarFixture
 
 BOOST_FIXTURE_TEST_SUITE(Car, CarFixture);
 
-BOOST_AUTO_TEST_CASE(CheckAllGears)
-{
-	BOOST_CHECK(car.TurnOnEngine());
-	BOOST_CHECK(car.SetSpeed(0));
-	BOOST_CHECK(!car.SetSpeed(5));
-	BOOST_CHECK(car.SetGear(REVERSE));
-	BOOST_CHECK(car.SetSpeed(2));
-	BOOST_CHECK(!car.SetGear(FIRST));
-	BOOST_CHECK(car.SetGear(NEUTRAL));
-	BOOST_CHECK(car.SetSpeed(0));
-	BOOST_CHECK(car.SetGear(FIRST));
-	BOOST_CHECK(car.SetSpeed(20));
-	BOOST_CHECK(car.SetGear(SECOND));
-	BOOST_CHECK(!car.SetGear(REVERSE));
-	BOOST_CHECK(car.SetSpeed(30));
-	BOOST_CHECK(car.SetGear(SECOND));
-	BOOST_CHECK(car.SetGear(THIRD));
-	BOOST_CHECK(car.SetSpeed(60));
-	BOOST_CHECK(car.SetGear(FOUTH));
-	BOOST_CHECK(car.SetGear(FIFTH));
-	BOOST_CHECK(car.SetGear(NEUTRAL));
-	
-
-
-}
-
-
 BOOST_AUTO_TEST_CASE(CheckDefaultState)
 {
 	BOOST_CHECK(!car.IsEngineTurnedOn());
 	BOOST_CHECK(car.GetSpeed() == 0);
 	BOOST_CHECK(car.GetGear() == NEUTRAL);
+	BOOST_CHECK(car.GetDir() == NEUTRAL);
 }
 
 BOOST_AUTO_TEST_CASE(TurningOnAndOff)
@@ -69,10 +44,95 @@ BOOST_AUTO_TEST_CASE(CantChangeIfTurnedOff)
 	BOOST_CHECK(!car.SetSpeed(1));
 }
 
+BOOST_AUTO_TEST_CASE(CantIncSpeedAndChangeGearOnNeurtal)
+{
+	BOOST_CHECK(car.TurnOnEngine());
+	BOOST_CHECK(car.SetGear(FIRST));
+	BOOST_CHECK(car.SetSpeed(20));
+	BOOST_CHECK(car.SetGear(NEUTRAL));
+	BOOST_CHECK(!car.SetSpeed(21));
+	BOOST_CHECK(car.GetSpeed() == 20);
+	BOOST_CHECK(car.SetSpeed(10));
+	BOOST_CHECK(car.GetSpeed() == 10);
+	BOOST_CHECK(!car.SetGear(FIRST));
+	BOOST_CHECK(car.GetGear() == NEUTRAL);
+
+}
+
+BOOST_AUTO_TEST_CASE(CantIncSpeed)
+{
+	BOOST_CHECK(car.TurnOnEngine());
+	BOOST_CHECK(car.SetGear(FIRST));
+	BOOST_CHECK(car.SetSpeed(20));
+	BOOST_CHECK(car.GetSpeed() == 20);
+	BOOST_CHECK(!car.SetSpeed(35));
+	BOOST_CHECK(car.GetSpeed() == 20);
+
+}
+
 BOOST_AUTO_TEST_CASE(CanChangeToItself)
 {
 	BOOST_CHECK(car.SetSpeed(0));
 	BOOST_CHECK(car.SetGear(NEUTRAL));
+}
+
+BOOST_AUTO_TEST_CASE(SimpleMoveCar)
+{
+	BOOST_CHECK(!car.SetGear(1));
+
+	BOOST_CHECK(car.TurnOnEngine());
+	BOOST_CHECK(!car.SetGear(111));
+	BOOST_CHECK(car.SetGear(1));
+	BOOST_CHECK(car.SetGear(1));
+	BOOST_CHECK(car.SetSpeed(30));
+	BOOST_CHECK(car.GetDir() == FORWARD);
+
+	BOOST_CHECK(!car.SetSpeed(60));
+	BOOST_CHECK(car.SetGear(3));
+	BOOST_CHECK(car.SetSpeed(60));
+	BOOST_CHECK(car.GetDir() == FORWARD);
+
+	BOOST_CHECK(!car.TurnOffEngine());
+}
+
+BOOST_AUTO_TEST_CASE(MovingBackwardAndForward)
+{
+	car.TurnOnEngine();
+	BOOST_CHECK(car.SetGear(6));
+
+	BOOST_CHECK(car.SetGear(1));
+	BOOST_CHECK(car.SetGear(6));
+
+	BOOST_CHECK(car.SetGear(6));
+	BOOST_CHECK(car.SetSpeed(20));
+	BOOST_CHECK(car.GetDir() == BACKWARD);
+
+	BOOST_CHECK(!car.SetGear(1));
+	BOOST_CHECK(car.SetSpeed(0));
+	BOOST_CHECK(car.SetGear(1));
+	BOOST_CHECK(car.GetDir() == NEUTRAL);
+
+	BOOST_CHECK(car.SetSpeed(10));
+	BOOST_CHECK(car.GetDir() == FORWARD);
+}
+
+BOOST_AUTO_TEST_CASE(MovingBackwardToNeutralOnSpeed)
+{
+	car.TurnOnEngine();
+	BOOST_CHECK(car.SetGear(6));
+	BOOST_CHECK(car.SetSpeed(20));
+
+	BOOST_CHECK(!car.SetGear(1));
+	BOOST_CHECK(car.SetGear(0));
+
+	BOOST_CHECK(!car.SetGear(1));
+	BOOST_CHECK(car.SetSpeed(0));
+	BOOST_CHECK(car.SetGear(1));
+
+	BOOST_CHECK(car.SetSpeed(10));
+	BOOST_CHECK(!car.SetGear(6));
+	BOOST_CHECK(car.SetSpeed(0));
+	BOOST_CHECK(car.SetGear(6));
 }
 
 
